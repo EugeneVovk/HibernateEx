09ce7c39c4b74f9edfae7d9576e18a59246ad283
Eugene Vovk <eugenevovk@gmail.com>
2021-08-12 18:19:40 +0300

    add Service

--- CoreTaskTemplate/src/main/java/jm/task/core/jdbc/util/Util.java
+++ CoreTaskTemplate/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,21 +6,22 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    /*
+
     private static final String URL = "jdbc:mysql://localhost:3306/jm_db";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
-     */
-    private static SessionFactory sessionFactory;
 
-    /*
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
-     */
+
 
     /*
     public static SessionFactory createSessionFactory() {
